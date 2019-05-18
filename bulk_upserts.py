37749759b455c6513979919cc4f9b59973d85675
Ruben Helsloot <rht670@student.vu.nl>
2019-05-18 14:07:07 +0200

    Add no-flush bulk upsert job

--- bulk_upserts.py
+++ bulk_upserts.py
@@ -127,6 +127,37 @@ def test_customer_batched_orm_select_add_all(n):
   session.commit()
 
 @Profiler.profile
+def test_customer_batched_orm_select_add_all_no_flush(n):
+  """
+  UPSERT statements via batched checks on whether objects exist
+  and add new objects in bulk - without flushing
+  """
+  session = Session(bind=engine)
+  for chunk in range(0, n, 1000):
+    customers = {
+        c.id: c for c in
+        session.query(Customer)\
+            .filter(Customer.id.between(chunk, chunk + 1000))
+    }
+    to_add = []
+    for i in range(chunk, chunk + 1000):
+      if i in customers:
+        customers[i].description += "updated"
+      else:
+        to_add.append({
+            "id": i,
+            "name": "customer name %d" % i,
+            "description": "customer description %d new" % i,
+        })
+    if to_add:
+      session.bulk_insert_mappings(
+        Customer,
+        to_add
+      )
+      to_add = []
+  session.commit()
+
+@Profiler.profile
 def test_customer_batched_orm_merge_result(n):
   "UPSERT statements using batched merge_results"
   session = Session(bind=engine)
@@ -134,11 +165,11 @@ def test_customer_batched_orm_merge_result(n):
     customers = session.query(Customer)\
         .filter(Customer.id.between(chunk, chunk + 1000))
     customers.merge_result(
-      Customer(
-          id=i,
-          name=f"customer name {i}",
-          description=f"customer description {i} new"
-      ) for i in range(chunk, chunk + 1000)
+        Customer(
+            id=i,
+            name=f"customer name {i}",
+            description=f"customer description {i} new"
+        ) for i in range(chunk, chunk + 1000)
     )
     session.flush()
   session.commit()
